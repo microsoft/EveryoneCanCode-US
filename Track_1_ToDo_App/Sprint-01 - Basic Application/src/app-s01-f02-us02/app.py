6b92865f09bdaa7b70b3e5dbc04c7f0cd26e7d64
Jeff Fattic <jfattic@microsoft.com>
2024-05-11 12:34:07 -0500

    removed unnecessary 'pass' keyword and made comments more consistent

--- Track_1_ToDo_App/Sprint-01 - Basic Application/src/app-s01-f02-us02/app.py
+++ Track_1_ToDo_App/Sprint-01 - Basic Application/src/app-s01-f02-us02/app.py
@@ -5,41 +5,41 @@
 
 todo_list = []
 
-# Load the to-do list from a file
+# load the to-do list from a file
 try:
     with open("todo_list.txt") as file:
         for line in file:
             todo_list.append(line.strip())
 except FileNotFoundError:
-    print("No saved items found")
-    pass #ignore the error if the file does not exist, simply start with an empty list
+    # ignore the error if the file does not exist, simply start with an empty list
+    print("No saved items found") 
 
-#continue to loop and display menu until user selects to exit the program
+# continue to loop and display menu until user selects to exit the program
 while True:
-    print() # Add a couple of blank lines
+    print() # add a couple of blank lines
     print()
-    print("To-do list: ") # Print the title of the list
+    print("To-do list: ") # print the title of the list
     item_number = 1
-    for todo in todo_list: # Loop through existing to-do items
+    for todo in todo_list: # loop through existing to-do items
         print(f'{item_number}: {todo}')
         item_number += 1
 
-# Print the menu
+# print the menu
     print() # Add a of blank lines
     print("Actions:")
     print("A - Add to-do item")
     print("R - Remove to-do item")
     print("X - Exit")
     choice = input("Enter your choice (A, R, or X): ")
-    choice = choice.upper() #converts the choice to uppercase
+    choice = choice.upper() # converts the choice to uppercase
 
-    #user selected 'a' or 'A' - To Add an item to the list
+    # user selected 'a' or 'A' to add an item to the list
     if choice == "A":
         todo = input("Enter the to-do item: ")
         todo_list.append(todo)
-        continue  #tells the program to go back to the start of the loop
+        continue  # tells the program to go back to the start of the loop
 
-    #user selected 'r' or 'R' - To Remove an item from the list
+    # user selected 'r' or 'R' - To Remove an item from the list
     if choice == "R":
         item_number = int(input("Enter the number of the item to remove: "))
         if item_number > 0 and item_number <= len(todo_list):
@@ -48,15 +48,15 @@ while True:
             print("Invalid item number")
         continue
 
-    #user 1 selected 'x' or 'X' to exit program
+    # user 1 selected 'x' or 'X' to exit program
     if choice == "X":
-        #on exit save your current list to a file
+        # on exit save your current list to a file
         print("Saving to-do list to file")
         with open("todo_list.txt", "w") as file:
             for todo in todo_list:
                 file.write(todo + "\n")
 
-        break #tells the program to exit the loop
+        break # tells the program to exit the loop
 
     #user selected something else
     print("Invalid choice")
